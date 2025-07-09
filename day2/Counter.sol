d5677b395f641cc5aabc7e206b4e0f3a7cba1322
Txh <txh5188@gmail.com>
2025-07-09 15:00:51 +0800

    day_2

--- day2/Counter.sol
+++ day2/Counter.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.20;
 contract Counter {
     int counter;
 
+    constructor() {
+        counter = 0;
+    }
+
     function get() public view returns (int) {
         return counter;
     }
